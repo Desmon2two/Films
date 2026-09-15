a532839b52f3baed64c569ee3300e10b594e3ae7
Desmon2 <alexandrmacris@gmail.com>
2026-09-15 13:58:01 +0300

    everything works, minor styling changes to accomodate dark theme

--- backend/src/authFeature/authController.js
+++ backend/src/authFeature/authController.js
@@ -81,7 +81,6 @@ async function getCurrentUser(req, res, next) {
 }
 async function patchUser(req, res, next) {
 	try {
-    console.log("BODY:", req.body)
 		const { userId } = req.user;
 		const userData = req.body;
 		const result = await userUseCase.patchUser(userId, userData);
